c9f41612fa93123a7ebf615c563174a5020ca766
Aaron Howser <aaronhowser1@gmail.com>
2022-08-27 13:46:22 -0500

    Move made into a record class

--- src/main/java/com/aaronhowser1/chess/logic/Move.java
+++ src/main/java/com/aaronhowser1/chess/logic/Move.java
@@ -1,19 +1,4 @@
 package com.aaronhowser1.chess.logic;
 
-public class Move {
-    private int x;
-    private int y;
-
-    public Move(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
+public record Move(int x, int y) {
 }
